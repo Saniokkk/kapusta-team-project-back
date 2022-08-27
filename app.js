544e67cc7136671b391d1275f6b635b6698eaa71
Aleksandr Serebro <aleksandrserebroz@gmail.com>
2022-08-27 23:51:46 +0300

    add start swager

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 require("dotenv").config();
 
@@ -17,6 +19,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api/users", authRouter);
 // app.use("/api/googleauth", googleRouter);
 app.use("/api/balance", balanceRouter);
