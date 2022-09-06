3f913a56cf507dcf0536a351778f32cdaab4e9e6
TkachikNataly <tkachik.nataly@gmail.com>
2022-09-06 03:04:05 +0300

    branch8back

--- controllers/GoogleAuthController.js
+++ controllers/GoogleAuthController.js
@@ -36,7 +36,7 @@ const googleRedirect = async (req, res) => {
     data: {
       client_id: process.env.GOOGLE_CLIENT_ID,
       client_secret: process.env.GOOGLE_CLIENT_SECRET,
-      redirect_uri: `${process.env.BASE_URL}/api/auth/google-redirect`,
+      redirect_uri: `${baseUrlBack}/api/auth/google-redirect`,
       grant_type: 'authorization_code',
       code,
     },
