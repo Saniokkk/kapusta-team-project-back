1ff3495ec837d90cb158f42578229d397e871363
Aleksandr.Kashirin <saniok26@rambler.ru>
2022-09-13 17:26:29 +0300

    fix login

--- helpers/joiSchemas.js
+++ helpers/joiSchemas.js
@@ -1,25 +1,25 @@
-// const Joi = require("joi");
+const Joi = require("joi");
 
-// const regDate = /^\d+$/;
-// const regDay = /(20[0-9][0-9])\-(0[1-0]|1[12])\-(0[1-9]|[12][0-9]|3[01])/;
-// const regDay =  /(0?[1-9]|[12][0-9]|3[01])[\/\-\.](0?[1-9]|1[012])/;
-
-// const validateDay = Joi.object.keys({
-//     date: Joi.string.pattern(regDay).length(10).required()
-// }).required();
-
-// const validateType = Joi.object.keys({
-//     type: Joi.string.valid('income', 'expense').required()
-// }).required();
+const regDate = /^\d+$/;
+const regDay = /(20[0-9][0-9])-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])/;
 
 
-// const validateMonthAndYear = Joi.object.keys({
-//     month: Joi.string.pattern(regDate).maxLength(2).required(),
-//     year: Joi.string.pattern(regDate).maxLength(4).required()
-// }).required()
+const validateDay = Joi.object.keys({
+    date: Joi.string.pattern(regDay).length(10).required()
+}).required();
 
-// module.exports = {
-//     validateDay,
-//     validateType,
-//     validateMonthAndYear
-// }
+const validateType = Joi.object.keys({
+    type: Joi.string.valid('income', 'expense').required()
+}).required();
+
+
+const validateMonthAndYear = Joi.object.keys({
+    month: Joi.string.pattern(regDate).maxLength(2).required(),
+    year: Joi.string.pattern(regDate).maxLength(4).required()
+}).required()
+
+module.exports = {
+    validateDay,
+    validateType,
+    validateMonthAndYear
+}
